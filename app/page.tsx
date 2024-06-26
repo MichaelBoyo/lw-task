afdf2ee4fe26e94179f324f796cf5b198037ae46
Michael Boyo <boyomichaelbidemi@gmail.com>
2024-06-27 00:03:22 +0100

    refactor: Add button component and update graph data layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,43 +2,72 @@ import { GraphData } from "@/components/graph-data";
 import { Card } from "@/components/ui/card";
 import { MAIN_CARDS_DATA, SECONDARY_CARDS_DATA } from "@/constants/dummy.data";
 import { GanttChartSquare } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const DashBoard = () => {
   return (
     <div className="space-y-4">
-      <div className="grid grid-cols-3 gap-4">
-        {MAIN_CARDS_DATA.map((val, index) => (
-          <Card key={index} className="p-4">
-            <div className="space-y-2">
-              <p>{val.title}</p>
-              <p>{val.rating}</p>
+      <div className="grid grid-cols-5 gap-4">
+        <div className="col-span-4 space-y-4">
+          <SectionOne />
+          <SectionTwo />
+          <GraphData />
+        </div>
+        <div>
+          <Card className="bg-black text-white flex flex-col items-center space-y-4 py-6">
+            <Image src="/Asset 3 1.png" height={300} width={100} alt="spiral" />
+
+            <div className=" text-center">
+              <p>Client Attendance Predictors</p>
+              <p>Here to be of service</p>
             </div>
-            <GanttChartSquare />
+
+            <p>Coming Soon</p>
           </Card>
-        ))}
+        </div>
       </div>
-      <div className="grid grid-cols-3 gap-4">
-        {SECONDARY_CARDS_DATA.map((val, index) => (
-          <Card key={index} className="p-4">
-            <div className="space-y-2">
-              <p>{val.title}</p>
-              <p>{val.rating}</p>
-            </div>
-            <GanttChartSquare />
-            <div>
-              {val.data.map((subData, id) => (
-                <Card key={id}>
-                  <p>{subData.name}</p>
-                </Card>
-              ))}
-            </div>
-          </Card>
-        ))}
-      </div>
-      <GraphData />
     </div>
   );
 };
 
 export default DashBoard;
+
+const SectionOne = () => {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {MAIN_CARDS_DATA.map((val, index) => (
+        <Card key={index} className="p-4">
+          <div className="space-y-2">
+            <p>{val.title}</p>
+            <p>{val.rating}</p>
+          </div>
+          <GanttChartSquare />
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+const SectionTwo = () => {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {SECONDARY_CARDS_DATA.map((val, index) => (
+        <Card key={index} className="p-4">
+          <div className="space-y-2">
+            <p>{val.title}</p>
+            <p>{val.rating}</p>
+          </div>
+          <GanttChartSquare />
+          <div>
+            {val.data.map((subData, id) => (
+              <Card key={id}>
+                <p>{subData.name}</p>
+              </Card>
+            ))}
+          </div>
+        </Card>
+      ))}
+    </div>
+  );
+};
--- components/graph-data.tsx
+++ components/graph-data.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "./ui/card";
 import { Info, MoreVertical, Repeat } from "lucide-react";
+import { Button } from "./ui/button";
 
 export const GraphData = () => {
   return (
@@ -13,6 +14,44 @@ export const GraphData = () => {
           <MoreVertical />
         </div>
       </div>
+
+      <div className="flex space-x-2 w-full justify-between">
+        {tabs.map((tab, i) => (
+          <Button key={i}>{tab}</Button>
+        ))}
+      </div>
+      <div className="flex space-x-6">
+        {chartsKeys.map((key, i) => (
+          <div key={i} className="flex items-center space-x-2">
+            <div
+              style={{ background: key.color }}
+              className="h-4 w-4 rounded-full"
+            />
+            <h1>{key.name}</h1>
+          </div>
+        ))}
+      </div>
     </Card>
   );
 };
+
+const tabs = ["Sleep", "Steps", "Weight", "Termerature"];
+
+const chartsKeys = [
+  {
+    name: "REM",
+    color: "yellow",
+  },
+  {
+    name: "Light Sleep",
+    color: "yellow",
+  },
+  {
+    name: "Deep Sleep",
+    color: "green",
+  },
+  {
+    name: "Awake",
+    color: "blue",
+  },
+];
